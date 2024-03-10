4c1e76096c76fdc2f11d5da03ece68cadaef025c
teacher <sbyteacademy@gmail.com>
2024-03-10 20:13:09 +0200

    create a job class

--- app/Jobs/DailyEmails.php
+++ app/Jobs/DailyEmails.php
@@ -2,29 +2,35 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\Attributes\WithoutRelations;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Nette\Utils\Image;
 
-class DailyEmails implements ShouldQueue
-{
+class DailyEmails implements ShouldQueue, ShouldBeUnique {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private User $user;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
-    {
-        //
+    public function __construct(User $user) {
+
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
-    {
-        //
+    public function handle(): void {
     }
+
+
 }
